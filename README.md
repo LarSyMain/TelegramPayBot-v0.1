dccd31b58f5954603e7338fc3c8d384793597df2
LarSyMain <96264278+LarSyMain@users.noreply.github.com>
2023-01-06 02:32:12 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# TelegramPayBot
+
+The bot was created to help people running telegram channels sell goods/services.
+
+One of the main tasks is to reduce the interaction of the buyer and seller.
+___
+## Libraries:
+1. telebot (pip install telebot)
+2. DonationalertsAPI (pip install DonationAlertsAPI)
+3. Requests (pip install requests)
+
+___
+## Payment system:
+The donationalersts system is used for payment, maybe you need an account on the site.
+
+
+
+
+
+
+Good Luck!
---
+++ main.py
@@ -0,0 +1,78 @@
+import telebot
+from telebot import types
+from donationalerts import Alert
+from requests import get
+
+
+bottoken = "5745372638:AAERoHCoc9pkWbdVpP4eVP79MzAwch46k7k"  #token
+bot = telebot.TeleBot(bottoken)
+
+tg_user_id = "892534199"
+da_alert_widget_token = "aAgqdBrpqBnkImjetPpy"
+
+
+Tname = ""
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    markup = types.ReplyKeyboardMarkup()
+    hhelp = types.KeyboardButton("/help")
+    global Tname
+    Tname = str(message.chat.username)
+    print(Tname)
+    markup.add(hhelp)
+    bot.send_message(message.chat.id, "Данный бот создан для помощи в оплате товаров и услуг.",reply_markup=markup, parse_mode="html") #старт и пояснение
+
+@bot.message_handler(commands=["help"])
+def help(massage):
+    markup = types.ReplyKeyboardMarkup()
+    fbuy = types.KeyboardButton("/buy") # ссылка на оплату
+    hstart = types.KeyboardButton("/start") #старт
+    hhelp = types.KeyboardButton("/help") #вызов кнопок
+    check = types.KeyboardButton("/check") #кнопка проверки покупки
+    markup.add(fbuy, hstart, hhelp, check)                                                                                           #кнопки для помощи
+    bot.send_message(massage.chat.id, "help button ↓", reply_markup=markup)        
+
+@bot.message_handler(commands=["buy"])
+def Buy(massage):
+    markup = types.InlineKeyboardMarkup()
+
+    markup.add(types.InlineKeyboardButton("Buy VIP", url = "https://www.donationalerts.com/r/larsymain"))
+    bot.send_message(massage.chat.id, "для оплаты перейдите по ссылке (!при покупке укажите свой telegram username!):", reply_markup=markup ,parse_mode="html")           #ссылка на покупку
+
+
+
+
+name = ""
+_sum_ = ""
+
+alert = Alert(da_alert_widget_token)
+@alert.event()
+def new_donation(event): 
+       get(f'https://api.telegram.org/bot{bottoken}/sendMessage?chat_id={tg_user_id}&text=Новый донат:\n{event.username} - {event.amount} {event.currency}\n{event.message}')
+       global name
+       name = str({event.username})
+       name = name[2:][:-2]
+
+       global _sum_
+       _sum_ = str({event.amount})                  #50 RUB
+       _sum_ = _sum_[2:]
+       _sum_ = _sum_[:-5]
+       
+       
+       print(name, _sum_)
+
+
+
+
+
+@bot.message_handler(commands=["check"])
+def check(massage):
+    if Tname == name and _sum_ == "50":
+        bot.send_message(massage.chat.id, "ссылка на вход: https://t.me/+t8PFxUkd9thlODhi" ,parse_mode="html") #проверка оплаты----------------------
+    else:
+        bot.send_message(massage.chat.id, "оплата не проведена" ,parse_mode="html")
+
+
+bot.polling(none_stop  = True)
+
